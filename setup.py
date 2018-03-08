6b25b5d60f06751cf9923f6662b07f849fe77021
Kiran Wattamwar <kiranwattamwar@Kirans-MacBook-Pro.local>
2018-03-07 21:28:00 -0500

    Trials for testing

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from time import gmtime, strftime, sleep, time
 from threading import Timer
+import os
 import random
+from trials import *
 
 # Each process communicates with other processes, and maintains its own logical clock
 # Processes are initiated with a speed, and randomly execute actions at regular intervals
 # based on their speeds.
 class Process:
-	def __init__(self, speed, id, log_file_path):
+	def __init__(self, speed, operation_cutoffs, id, log_file_path):
 		self.logical_clock = 0
 		self.speed = speed
 		self.id = id
@@ -14,9 +16,9 @@ class Process:
 		self.message_queue = []
 		self.timer = None
 		self.on = True
+		self.send_1, self.send_2, self.both_messages = operation_cutoffs
 		print("Machine %d initialized with %d operations per second" % (id, speed))
 
-	# THIS NEEDS TO BE DEBUGGED
 	# Originally, I just had the last line and instead of self.start it was calling self.handle_operation, but it wouldnt repeat the function so I dumped it in here and made it recursive
 	def start(self):
 		if self.on:
@@ -24,15 +26,15 @@ class Process:
 			if len(self.message_queue) == 0:
 				opcode = random.randint(1,7)
 				# Send to to one machine
-				if opcode == 1:
+				if opcode <= self.send_1:
 					target_id = (self.id + 1) % 3
 					self.send_single_message(target_id)
 				# Send to the other machine
-				elif opcode == 2:
+				elif opcode <= self.send_2:
 					target_id = (self.id - 1) % 3
 					self.send_single_message(target_id)
 				# Send to both machines
-				elif opcode == 3:
+				elif opcode <= self.both_messages:
 					target_1 = (self.id + 1) % 3
 					target_2 = (self.id - 1) % 3
 					self.send_multiple_messages(target_1, target_2)
@@ -43,30 +45,6 @@ class Process:
 				message = self.message_queue.pop(0)
 				self.receive(message)
 			self.timer = Timer(1.0/self.speed, self.start).start()
-
-	# at a regular interval, generate a random number and execute
-	# def handle_operation(self):	
-	# 	if len(self.message_queue) == 0:
-	# 		opcode = random.randint(1,7)
-	# 		# Send to to one machine
-	# 		if opcode == 1:
-	# 			target_id = (self.id + 1) % 3
-	# 			self.send_single_message(target_id)
-	# 		# Send to the other machine
-	# 		elif opcode == 2:
-	# 			target_id = (self.id - 1) % 3
-	# 			self.send_single_message(target_id)
-	# 		# Send to both machines
-	# 		elif opcode == 3:
-	# 			target_1 = (self.id + 1) % 3
-	# 			target_2 = (self.id - 1) % 3
-	# 			self.send_multiple_messages(target_1, target_2)
-	# 		# Internal event, update local clock
-	# 		else:
-	# 			self.internal_event()
-	# 	else:
-	# 		message = self.message_queue.pop(0)
-	# 		self.receive(message)
 
 	# Append a message to queue to buffer a received message
 	def append_to_message_queue(self, message):
@@ -105,24 +83,31 @@ class Process:
 
 
 if __name__== "__main__":
-	print("Starting to create machines")
-	machines = {}
-	machines[0] = Process(random.randint(1,7), 0, "log_0.log")
-	machines[1] = Process(random.randint(1,7), 1, "log_1.log")
-	machines[2] = Process(random.randint(1,7), 2, "log_2.log")
+	trial = 0
+	for speeds in all_speeds:
+		for probs in all_probs:
+			os.mkdir("trial%d"%trial)
+			print("Starting to create machines")
+			print("Speeds: " + str(speeds))
+			print("Probs: " + str(probs))
+			machines = {}
+			machines[0] = Process(speeds[0], probs, 0, "trial%d/log_0.log"%trial)
+			machines[1] = Process(speeds[1], probs, 1, "trial%d/log_1.log"%trial)
+			machines[2] = Process(speeds[2], probs, 2, "trial%d/log_2.log"%trial)
 
-	machines[0].start()
-	machines[1].start()
-	machines[2].start()
+			machines[0].start()
+			machines[1].start()
+			machines[2].start()
 
-	try:
-		# Run for 1 minute
-		sleep(60)
-		
-	finally:
-		# Stop them from running
-		machines[0].stop()
-		machines[1].stop()
-		machines[2].stop()
-		print("1 minute has elapsed. Logs are generated in log_<id>.log")
-		print("To see plots, run 'python plots.py'")
+			try:
+				# Run for 1 minute
+				sleep(60)
+				
+			finally:
+				# Stop them from running
+				machines[0].stop()
+				machines[1].stop()
+				machines[2].stop()
+				print("1 minute has elapsed. Logs are generated in trials%d/log_<id>.log"%trial)
+				trial += 1
+				print("Run plotter separately to see output.")
